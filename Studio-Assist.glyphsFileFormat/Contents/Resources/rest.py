e6ed1e1b75c7ef941f13f8b520eeb2f6079f0cc2
Bryan Comeau <nhboy42@gmail.com>
2024-09-24 18:40:13 -0400

    Ongoing changes

--- Studio-Assist.glyphsFileFormat/Contents/Resources/rest.py
+++ Studio-Assist.glyphsFileFormat/Contents/Resources/rest.py
@@ -62,6 +62,7 @@ class Api(object):
             response = requests.get(url, timeout=(self.connect_timeout, self.read_timeout))
             response.raise_for_status()  # Raise an exception for non-2xx status codes
             self.networkLog.info(f"Response: {response.status_code}") 
+            self.networkLog.info(f"Response: {response.text}")
             return response.status_code
 
         except requests.exceptions.RequestException as e:
@@ -114,14 +115,14 @@ class Api(object):
     def poll_for_completion(self, poll_url, font_id, interval=5, timeout=60):
 
         self.networkLog.info(f"poll_for_completion url {poll_url} font id {font_id}") 
-        payload = {"font_id": font_id}
+        params = {"font_id": font_id}
 
 
         start_time = time.time()
 
         while True:
             try:
-                response = requests.get(poll_url, params=payload, timeout=(self.connect_timeout, self.read_timeout))
+                response = requests.get(poll_url, params=params, timeout=(self.connect_timeout, self.read_timeout))
                 response.raise_for_status()  # Raise an exception for non-2xx status codes
                 self.networkLog.info(f"Response: {response.status_code}") 
                 return response.status_code
@@ -150,10 +151,10 @@ class Api(object):
     def get_genai_font_zip(self, fetch_font_url, path_to_zip, font_id, unicode_string):
 
         self.networkLog.info(f"get_genai_font_zip()  url {fetch_font_url} zip to path {path_to_zip} font id {font_id} unicode string {unicode_string}") 
-        payload = {"font_id": font_id, "unicode_string": unicode_string}
+        params = {"font_id": font_id, "unicode_string": unicode_string}
 
         try:
-            response = requests.get(fetch_font_url, params=payload, stream=True, timeout=(self.connect_timeout, self.read_timeout))
+            response = requests.get(fetch_font_url, params=params, stream=True, timeout=(self.connect_timeout, self.read_timeout))
             self.networkLog.info(f"Response: {response.status_code}") 
             response.raise_for_status()    # Raise an exception for non-2xx status codes
 
